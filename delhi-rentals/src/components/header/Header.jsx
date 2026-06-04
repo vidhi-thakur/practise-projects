2586641747a9449fea603aff8e1a949e6ba15c0d
Vidhi Thakur <thakurvidhi997@gmail.com>
2026-06-04 19:34:00 +0530

    added a responsive header component

--- delhi-rentals/src/components/header/Header.jsx
+++ delhi-rentals/src/components/header/Header.jsx
@@ -1,7 +1,25 @@
-// import styles from './Header.module.css';
+import styles from "./Header.module.css";
+import { House, Phone, Mail } from "lucide-react";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <header className={styles.header}>
+      <div className={styles.box}>
+        <House size={20} />
+        <span style={{ fontWeight: "bold", fontSize: "1.5rem" }}>
+          Delhi Rentals
+        </span>
+      </div>
+      <div className={styles.box}>
+        <Phone size={20} />
+        <span>62937393848</span>
+      </div>
+      <div className={styles.box}>
+        <Mail size={20} />
+        <span>rentals.delhi@gmail.com</span>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
