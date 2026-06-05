e489fa7d4e196211ac756f58902a52b332c802b0
Vidhi Thakur <thakurvidhi997@gmail.com>
2026-06-05 15:04:23 +0530

    added title

--- delhi-rentals/src/components/title/Title.jsx
+++ delhi-rentals/src/components/title/Title.jsx
@@ -1,7 +1,11 @@
-// import styles from './Title.module.css';
+import styles from './Title.module.css';
 
 const Title = () => {
-  return <div></div>;
+  return (
+    <div className={styles.container}>
+      <h1>Properties for rent</h1>
+    </div>
+  );
 };
 
 export default Title;
